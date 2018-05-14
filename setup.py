8f81269e48f8833934316406629313a91aac2917
Jakub Janarek <jjanarek@gmail.com>
2018-05-14 13:39:25 +0200

    prepare setup.py for uploading to pypi with twine

--- setup.py
+++ setup.py
@@ -1,41 +1,126 @@
 #!/usr/bin/env python
-from setuptools import setup
+# -*- coding: utf-8 -*-
 
-VERSION = '2.0.0'
-DESCRIPTION = "PyBloom: A Probabilistic data structure"
-LONG_DESCRIPTION = """
-pybloom is a Python implementation of the bloom filter probabilistic data
-structure. The module also provides a Scalable Bloom Filter that allows a
-bloom filter to grow without knowing the original set size.
-"""
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
 
-CLASSIFIERS = filter(None, map(str.strip,
-"""
-Intended Audience :: Developers
-License :: OSI Approved :: MIT License
-Programming Language :: Python
-Programming Language :: Python :: 3",
-Operating System :: OS Independent
-Topic :: Utilities
-Topic :: Database :: Database Engines/Servers
-Topic :: Software Development :: Libraries :: Python Modules
-""".splitlines()))
+import io
+import os
+import sys
+from shutil import rmtree
 
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'dm_pybloom'
+DESCRIPTION = 'Datamaran\'s fork of Pybloom for Python3
+URL = 'https://github.com/datamaranai/python-bloomfilter/'
+EMAIL = 'jjanarek@gmail.com'
+AUTHOR = 'jxub'
+REQUIRES_PYTHON = '>=3.5.0'
+VERSION = '3.0.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'bitarray>=0.3.4'
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="pybloom",
-    version=VERSION,
+    name=NAME,
+    version=about['__version__'],
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    classifiers=CLASSIFIERS,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
     keywords=('data structures', 'bloom filter', 'bloom', 'filter',
               'probabilistic', 'set'),
-    author="Jay Baird",
-    author_email="jay.baird@me.com",
-    url="http://github.com/jaybaird/python-bloomfilter/",
-    license="MIT License",
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
     platforms=['any'],
     test_suite="pybloom.tests",
     zip_safe=True,
-    install_requires=['bitarray>=0.3.4'],
-    packages=['pybloom']
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
